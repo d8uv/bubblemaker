0716be02a8454c047ea901220aa83a32d63e5fb4
d8uv <github@d8uv.org>
2010-01-29 17:18:18 -0900

    ported the raphaelbranch's refactoring, gonna refactor more though (honestly for the sake of better minification (yes i know that's a stupid reason shut up))

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-"use strict"; /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true*/ /*global document*/
+"use strict"; /*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true*/ /*global document window*/
 window.onload = function () {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    
+ 
     function drawCircles() {
-        //clear canvas/setup colors
-        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        //setup canvas
+        var ctx = document.getElementById('canvas').getContext('2d');
+        ctx.canvas.width = document.body.clientWidth;
+        ctx.canvas.height = document.body.clientHeight;
         document.body.style.background = document.config.bg.value;
         document.body.style.color = document.config.fg.value;
         ctx.fillStyle = document.config.fg.value;
@@ -13,13 +14,13 @@ window.onload = function () {
         //outercircle
         ctx.globalAlpha = 1;
         ctx.beginPath();
-        ctx.arc(document.body.clientWidth / 2, document.body.clientHeight / 2, document.config.size.value / 2, 0, 2 * Math.PI, true);
+        ctx.arc(ctx.canvas.width / 2, ctx.canvas.height / 2, document.config.size.value / 2, 0, 2 * Math.PI, true);
         ctx.stroke();
         
         //inner circle
         ctx.globalAlpha = 0.5;
         ctx.beginPath(); 
-        ctx.arc(document.body.clientWidth / 2, document.body.clientHeight / 2, (document.config.size.value * document.config.percent.value) / 200, 0, 2 * Math.PI, true);
+        ctx.arc(ctx.canvas.width / 2, ctx.canvas.height / 2, (document.config.size.value * document.config.percent.value) / 200, 0, 2 * Math.PI, true);
         ctx.fill();
         
         //text
@@ -27,7 +28,7 @@ window.onload = function () {
         ctx.font = document.config.size.value / 3 + 'px sans-serif';
         ctx.textAlign = 'left';
         ctx.textBaseline = 'middle';
-        ctx.fillText(Math.round(document.config.percent.value) + "%", document.body.clientWidth / 2 + document.config.size.value * 1.1, document.body.clientHeight / 2);
+        ctx.fillText(Math.round(document.config.percent.value * 100) / 100 + "%", ctx.canvas.width / 2 + document.config.size.value * 0.6, ctx.canvas.height / 2);
     }
     
     drawCircles();
